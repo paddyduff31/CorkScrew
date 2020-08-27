f0199400d76f1f759d56129c650dd65d9dbdc78e
Paddy Duff <paddy.duff31@googlemail.com>
2020-08-27 14:28:34 +0100

    yarn removed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 const todoRoutes = express.Router()
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 let Todo = require('./todo.model')
 
