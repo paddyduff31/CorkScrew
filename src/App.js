b10465dced2f67de2a418bd21e64eeed2835803a
Paddy Duff <paddy.duff31@googlemail.com>
2020-08-27 12:38:54 +0100

    to do complete

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+
+
+import CreateTodo from "./Component/create-todo.component";
+import EditTodo from "./Component/edit-todos.component";
+import TodosList from "./Component/todos-list.component";
+import Tills from "./Component/Tills/tills.component";
+
+import logo from "./logo.png" 
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="patrickduff.digital" target="_blank">
+          <img src={logo} width="30" height="30" alt="Corckscrew Logo"></img>
+        </a>
+        <Link to="/" className="navbar-brand">CorckScrew</Link>
+        <div className="nav-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Todos</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Todo</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/tills" className="nav-link">Tills</Link>
+            </li>
+          </ul>
+
+        </div>
+        <Form inline className="ml-auto">
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+      </nav>
+      <Route path="/" exact component={TodosList}/>
+      <Route path="/edit/:id" component={EditTodo}/>
+      <Route path="/create" component={CreateTodo}/>
+      <Route path="/tills" component={Tills}/>
+    </div>
+    
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Component/Tills/tills.component.js
@@ -0,0 +1,168 @@
+import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+
+export default class CreateTodo extends Component {
+    render() {
+        return (
+            <div className="ml-3">
+                <Card className="text-center mt-1" border="primary"><Card.Title>Sweet Afton</Card.Title></Card>
+                
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <div className="text-center m-3" ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                </svg></div>
+                
+
+                <h4>Ritas</h4>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                    <svg className="float-right" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-dash-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M3.5 8a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5z"/>
+</svg>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <div className="text-center m-3" ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                </svg></div>
+                
+                <h4>Perch</h4>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <div className="text-center m-3" ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                </svg></div>
+
+                <h4>Franklins</h4>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <Form inline className="mt-2"> 
+                    <Form.Control placeholder="Cash" />
+                    <Form.Control placeholder="Card" />
+                    <Form.Control placeholder="Payouts" />
+                    <Form.Control placeholder="Food" />
+                    <Form.Control placeholder="Drink" />
+                    <Form.Control placeholder="Turnover" />
+                    <Button type="submit" className="my-1">
+                        Submit
+                    </Button>
+                </Form>
+                <div className="text-center m-3" ><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                </svg></div>
+            </div>
+        )
+    }
+}
